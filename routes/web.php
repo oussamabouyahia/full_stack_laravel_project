211ff3f9e0b4a4305954cc357f1b008cc2e6f954
unknown <oussemabenbouali.bouyahia@gmail.com>
2025-03-13 12:45:17 +0100

    implementing guest and auth middlewares and group routes to clean code

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/register",[authController::class,"showRegister"])->name("show.register");
-Route::get("/login",[authController::class,"showLogin"])->name("show.login");
-Route::post("/register",[authController::class,"register"])->name("register");
-Route::post("/login",[authController::class,"login"])->name("login");
+
 Route::post("/logout",[authController::class,"logout"])->name("logout");
 
+//guest
+Route::middleware('guest')->controller(authController::class)->group( function(){
+            Route::get("/register","showRegister")->name("show.register");
+            Route::get("/login","showLogin")->name("show.login");
+            Route::post("/register","register")->name("register");
+            Route::post("/login","login")->name("login");
+});
 
-Route::get('/ninjas', [ninjaController::class,"index"])->name('ninjas.index');
-Route::get('/ninjas/create', [ninjaController::class,"create"])->name('ninjas.create');
-Route::get('/ninjas/{ninja}', [ninjaController::class,"show"])->name('ninjas.show');
-Route::post('/ninjas', [ninjaController::class,"store"])->name('ninjas.store');
-Route::delete('/ninjas/{ninja}', [ninjaController::class,"destroy"])->name('ninjas.destroy');
+
+//authenticated
+Route::middleware('auth')->controller(ninjaController::class)-> group( function (){
+            Route::get('/ninjas', "index")->name('ninjas.index');
+            Route::get('/ninjas/create', "create")->name('ninjas.create');
+            Route::get('/ninjas/{ninja}', 'show')->name('ninjas.show');
+            Route::post('/ninjas', "store")->name('ninjas.store');
+            Route::delete('/ninjas/{ninja}', "destroy")->name('ninjas.destroy');
+});
